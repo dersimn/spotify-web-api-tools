af69d17bb0f8fe643e95aacd568abd93f24934ab
Simon Christmann <simon@christmann.email>
2021-03-13 11:20:25 +0100

    findUserPlaylistByName no match return undefined
    Signed-off-by: Simon Christmann <simon@christmann.email>

--- src/tools.js
+++ src/tools.js
@@ -105,7 +105,7 @@ class SpotifyWebApiTools {
         }
 
         if (playlistByName.length === 0) {
-            throw new Error('Could not find Playlist with Name: ' + playlistName);
+            return;
         }
 
         return playlistByName[0];
